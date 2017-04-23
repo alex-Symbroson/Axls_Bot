5afa2513cc0b5740fb1d95ff6b755ea4f66eda01
alex-Symbroson <alex.symbroson@gmail.com>
2017-04-23 03:03:24 +0200

    toInt function improved
    - def toInt moved to modules.py
    - removed toInt.py

--- TelegramBot/Modules.py
+++ TelegramBot/Modules.py
@@ -4,6 +4,7 @@ from json import loads, dumps
 from _random import Random
 from os import makedirs
 from re import sub
+from math import *
 
 random = Random()
 ranint = lambda m:int(m*random.random())
@@ -77,3 +78,39 @@ def toMsg(data):
     # return permission name by index
 def perm(i):
     return ['slave', 'normal', 'super', 'admin'][i]
+
+    # allowed functions
+allowed = ('abs|acos|acosh|asin|asinh|atan|atan2|atanh|ceil|copysign|cos|cosh|'
+           'degrees|e|erf|erfc|exp|expm1|fabs|factorial|f1|floor|fmod|frexp|'
+           'fsum|gamma|hypot|isfinite|isinf|isnan|ldexp|lgamma|log|log1|log2p|'
+           'modf|pi|radians|round|sin|sinh|sqr|sqrt|tan|tanh|trunc|W|\+|\-|\*|'
+           '\/|\^|\<|\>|\%|\&|\||\(|\)|[0-9]')
+
+sqr = lambda x:x**2
+
+    # Lambert W-function (why?! XD)
+    # inverted function to f(x)=x*e^x
+def W(x):
+        # y-result, d-digit as 10exponent, s-y*e^y, t-previous s
+    y,d,s,t = 0,-2,0,-1
+        # relation of s to x
+    f = r = 1 if y*pow(e,y)<x else -1
+    
+    while d<17:                 # python calculates with 16 float digits
+        while r==f:             # if relation of s to x changed - calc next digit
+            if s==t:return y    # return if y*e^y == result
+            t = s
+            if abs(y) > 10000:return "NaN"    # x not in W(x)
+            y += f*pow(10, -d)      # in/decrease a digit dependent on f
+            s = y*pow(e, y)         # calculate x of current y value
+            f = 1 if s < x else -1  # recalculate relation of s to x
+            
+        r, d = f, d+1
+    return y
+
+    # calculate int from string via eval
+def toInt(s):
+    if sub(allowed,"",s): return "not allowed!"
+    try: return eval(s)
+    except ZeroDivisionError: return 'division by zero!'
+    except: return 'Error!'
